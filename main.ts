1f1ba9019a82e3f07db3444b359de5155a9bd7ce
programatorovic <programatorovic@gmail.com>
2024-12-07 13:46:15 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ namespace drawLine {
     /**
      * Implementácia algoritmu kreslenia čiary.
      */
-    export function lineImplementation(x1: number, y1: number, x2: number, y2: number): void {
+    export function drawLineImplementation(x1: number, y1: number, x2: number, y2: number): void {
         let dx = Math.abs(x2 - x1);
         let dy = Math.abs(y2 - y1);
         let sx = (x1 < x2) ? 1 : -1;
@@ -10,8 +10,8 @@ namespace drawLine {
         let err = dx - dy;
 
         while (true) {
-            led.plot(x1, y1); // Zapni LED na súradniciach [x1, y1]
-            if (x1 === x2 && y1 === y2) break; // Ak sme dosiahli koncový bod, zastav
+            led.plot(x1, y1);
+            if (x1 === x2 && y1 === y2) break;
             let e2 = 2 * err;
             if (e2 > -dy) {
                 err -= dy;
